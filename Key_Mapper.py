139b693fd9c246266a7793f7e897c1dcb155329e
William E Hahn <williamedwardhahn@gmail.com>
2021-08-26 18:35:13 -0400

    Create Key_Mapper.py

---
+++ Key_Mapper.py
@@ -0,0 +1,35 @@
+# William Edward Hahn
+# August 2021
+import pygame
+import numpy as np
+
+
+pygame.init()
+font = pygame.font.SysFont('futura', 30)
+
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 100
+screen_height = 100
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('William Hahn\'s Key Mapper')
+
+def keys():
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+	return np.where(pygame.key.get_pressed())[0]
+
+
+run = True
+while run:
+
+	clock.tick(fps)
+
+	print(keys())
+
+	pygame.display.update()
+
+pygame.quit()
