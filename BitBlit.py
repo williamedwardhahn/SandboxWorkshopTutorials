fe03e03a53dbb41a40073b3fde59a7e9a54661b8
William E Hahn <williamedwardhahn@gmail.com>
2021-08-27 00:57:10 -0400

    Update BitBlit.py

--- BitBlit.py
+++ BitBlit.py
@@ -1,19 +1,15 @@
-import pygame
-import numpy as np
-from pygame import surfarray
+import os
+os.system("pip install --ignore-installed  git+https://github.com/williamedwardhahn/DeepZoo")
+from DeepZoo import *
 
-pygame.init()
+init()
 
-w = 256
-h = 256
-
-display = pygame.display.set_mode((w, h))
+display = setup(256,256)
 
 while True:
 
     img = 256*np.random.random((256,256,3))
-    
-    surfarray.blit_array(display, img)
-    pygame.display.flip()
 
-pygame.quit()
+    blit(display, img)
+
+quit()
