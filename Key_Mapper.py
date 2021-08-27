0c0e1492ce83560f0a7717e28220b2465e9085e5
William E Hahn <williamedwardhahn@gmail.com>
2021-08-27 00:09:40 -0400

    Update Key_Mapper.py

--- Key_Mapper.py
+++ Key_Mapper.py
@@ -1,42 +1,20 @@
 # William Edward Hahn
 # August 2021
-import pygame as pg
-import numpy as np
+import os
+os.system("pip install --ignore-installed  git+https://github.com/williamedwardhahn/DeepZoo")
+from DeepZoo import *
 
-pg.init()
-font = pg.font.SysFont('futura', 30)
+init()
 
-clock = pg.time.Clock()
-fps = 60
-
-screen_width = 100
-screen_height = 100
-
-screen = pg.display.set_mode((screen_width, screen_height))
-pg.display.set_caption('William Hahn\'s Key Mapper')
-
-def keys():
-	for event in pg.event.get():
-		if event.type == pg.QUIT:
-			run = False
-	return np.where(pg.key.get_pressed())[0]
-
-def mouse():
-	for event in pg.event.get():
-		if event.type == pg.QUIT:
-			run = False
-	return (pg.mouse.get_pos(), np.where(pg.mouse.get_pressed())[0])
-
+display = setup(256,256)
 
 run = True
 while run:
 
-	clock.tick(fps)
+	tick()
 
 	print(keys())
 
 	print(mouse())
 
-	pg.display.update()
-
-pg.quit()
+quit()
